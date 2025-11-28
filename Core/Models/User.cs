d4f0756dec535d6438be90c2070d1dd7e9031bfb
ernar kasen <kasen.ernar06@gmail.com>
2025-11-28 15:35:09 +0500

    User ID Fixes

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -1,7 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Core.Models
 {
     public class User
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = null!;
         public string Name { get; set; }
         public string Email { get; set; }
