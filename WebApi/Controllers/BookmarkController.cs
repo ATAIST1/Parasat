e3319a2b77c49a34bc683223d4b85e7aae3017b2
KUANAIT <230703@astanait.edu.kz>
2025-12-04 03:25:20 +0500

    bookmark auth functionality

--- WebApi/Controllers/BookmarkController.cs
+++ WebApi/Controllers/BookmarkController.cs
@@ -1,32 +1,41 @@
 using System.Threading.Tasks;
 using Application.Services;
 using Core.Dtos;
+using Core.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace WebApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
+[Authorize]
 public class BookmarkController : ControllerBase
 {
     private readonly BookmarkService _bookmarkService;
+    private readonly IHttpContextAccessor _httpContext;
 
-    public BookmarkController(BookmarkService bookmarkService)
+    public BookmarkController(BookmarkService bookmarkService, IHttpContextAccessor httpContext)
     {
         _bookmarkService = bookmarkService;
+        _httpContext = httpContext;
     }
 
-    [HttpGet("user/{userId}")]
-    public async Task<IActionResult> GetByUserAsync(string userId)
+    private string CurrentUserId => _httpContext.HttpContext?.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value 
+                          ?? throw new UnauthorizedAccessException("User is not authenticated");
+
+    [HttpGet("user")]
+    public async Task<IActionResult> GetByUserAsync()
     {
-        var bookmarks = await _bookmarkService.GetByUserAsync(userId);
+        var bookmarks = await _bookmarkService.GetByUserAsync(CurrentUserId);
         return Ok(bookmarks);
     }
 
     [HttpPost]
     public async Task<IActionResult> CreateAsync([FromBody] CreateBookmarkDto dto)
     {
-        var bookmark = await _bookmarkService.CreateAsync(dto);
+        var bookmark = await _bookmarkService.CreateAsync(dto, CurrentUserId);
         return Ok();
     }
 
@@ -38,9 +47,14 @@ public class BookmarkController : ControllerBase
     }
 
     [HttpDelete]
-    public async Task<IActionResult> DeleteByUserAndItemAsync([FromQuery] string userId, [FromQuery] string itemId, [FromQuery] string itemType)
+    public async Task<IActionResult> DeleteByUserAndItemAsync([FromQuery] string itemId, [FromQuery] int itemType)
     {
-        var deleted = await _bookmarkService.DeleteByUserAndItemAsync(userId, itemId, itemType);
+        if (!Enum.IsDefined(typeof(BookmarkItemType), itemType))
+        {
+            return BadRequest("Invalid itemType. Must be 0 (Startup), 1 (Investor), 2 (Developer), or 3 (Business)");
+        }
+        
+        var deleted = await _bookmarkService.DeleteByUserAndItemAsync(CurrentUserId, itemId, (BookmarkItemType)itemType);
         return deleted ? NoContent() : NotFound();
     }
 }
